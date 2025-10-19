3cd363dd7b0c59e5e47699f14fd43eb2e22bd154
Suravi Pubudu <suravipk@gmail.com>
2025-10-19 23:49:01 +1030

    ADD: Booking API FIX: filter casesensitive issue

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,23 +4,23 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.class.createMany({
     data: [
-      { type: "Bachata", level: 1, dayOfWeek: "Monday", startTime: "18:30", maxSpots: 20 },
-      { type: "Bachata", level: 2, dayOfWeek: "Monday", startTime: "19:30", maxSpots: 20 },
-      { type: "Salsa", level: 3, dayOfWeek: "Monday", startTime: "20:30", maxSpots: 20 },
+      { type: "bachata", level: 1, dayOfWeek: "Monday", startTime: "18:30", maxSpots: 20 },
+      { type: "bachata", level: 2, dayOfWeek: "Monday", startTime: "19:30", maxSpots: 20 },
+      { type: "salsa", level: 3, dayOfWeek: "Monday", startTime: "20:30", maxSpots: 20 },
 
-      { type: "Salsa", level: 1, dayOfWeek: "Tuesday", startTime: "18:30", maxSpots: 20 },
-      { type: "Salsa", level: 2, dayOfWeek: "Tuesday", startTime: "19:30", maxSpots: 20 },
-      { type: "Reggaeton", dayOfWeek: "Tuesday", startTime: "20:30", maxSpots: 20 },
+      { type: "salsa", level: 1, dayOfWeek: "Tuesday", startTime: "18:30", maxSpots: 20 },
+      { type: "salsa", level: 2, dayOfWeek: "Tuesday", startTime: "19:30", maxSpots: 20 },
+      { type: "reggaeton", dayOfWeek: "Tuesday", startTime: "20:30", maxSpots: 20 },
 
-      { type: "Bachata", level: 1, dayOfWeek: "Wednesday", startTime: "18:30", maxSpots: 20 },
-      { type: "Bachata", level: 2, dayOfWeek: "Wednesday", startTime: "19:30", maxSpots: 20 },
-      { type: "Salsa", level: 3, dayOfWeek: "Wednesday", startTime: "20:30", maxSpots: 20 },
+      { type: "bachata", level: 1, dayOfWeek: "Wednesday", startTime: "18:30", maxSpots: 20 },
+      { type: "bachata", level: 2, dayOfWeek: "Wednesday", startTime: "19:30", maxSpots: 20 },
+      { type: "salsa", level: 3, dayOfWeek: "Wednesday", startTime: "20:30", maxSpots: 20 },
 
-      { type: "Salsa", level: 1, dayOfWeek: "Thursday", startTime: "18:30", maxSpots: 20 },
-      { type: "Salsa", level: 2, dayOfWeek: "Thursday", startTime: "19:30", maxSpots: 20 },
-      
-      { type: "Salsa", level: 3, dayOfWeek: "Friday", startTime: "18:30", maxSpots: 20 },
-      { type: "Reggaeton", dayOfWeek: "Friday", startTime: "19:30", maxSpots: 20 },
+      { type: "salsa", level: 1, dayOfWeek: "Thursday", startTime: "18:30", maxSpots: 20 },
+      { type: "salsa", level: 2, dayOfWeek: "Thursday", startTime: "19:30", maxSpots: 20 },
+
+      { type: "salsa", level: 3, dayOfWeek: "Friday", startTime: "18:30", maxSpots: 20 },
+      { type: "reggaeton", dayOfWeek: "Friday", startTime: "19:30", maxSpots: 20 },
     ],
   });
 }
